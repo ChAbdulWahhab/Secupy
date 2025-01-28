097290f7e405cb1c2cf21645998c4b98bc39790a
oliver-dev01 <oliver.james.dev@gmail.com>
2025-01-28 05:31:46 +0500

    setup file updated.

--- setup.py
+++ setup.py
@@ -3,16 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name="secupy",
     version="0.1.0",
-    description="A Python library for secure encryption, robust hashing, reliable communication, and advanced asymmetric encryption.",
-    author="Ch. Abdul Wahab",
+    description="A comprehensive Python library for secure encryption, hashing, communication, and asymmetric encryption.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ch. Abdul Wahhab",
     author_email="ch.abdul.wahab310@gmail.com",
     url="https://github.com/ChAbdulWahhab/Secupy",
     packages=find_packages(),
-    install_requires=["cryptography", "bcrypt"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    install_requires=[
+        "cryptography>=3.4",
+        "bcrypt>=3.2"
+    ],
 )
